a97dd78c39b6c2f3f25f5f75f65537aa8f771ca2
dev2kod <devesh4coding@gmail.com>
2026-03-08 15:06:57 +0530

    added bcrypt package for hasing

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,10 @@ const authRouter = require("./routes/authRoutes")
 
 const app = express()
 app.use(express.json())
-
+ 
 app.use("/api/auth",require(authRouter))
+
+
 
 
 module.exports = app;
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,16 @@
 const express = require("express")
-
+const controller = require("../controller/authController")
 const authRouter = express.Router()
 
 // @route POST /api/auth/register
 // @desc Register a new user
 
-authRouter.post("/register",registerUser)
+authRouter.post("/register",controller.registerUserController)
 
+// @route POST /api/auth/login
+// @desc Login user and return JWT token
+
+authRouter.post("/login",LoginUser)
 
 
 
