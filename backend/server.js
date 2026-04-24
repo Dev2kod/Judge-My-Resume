392314b7fd2d8b3c043cf516dd6d53889240b8bd
dev2kod <devesh4coding@gmail.com>
2026-04-24 23:55:07 +0530

    restarting

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,4 @@
 require("dotenv").config();
-const connectDb = require("./src/config/db");
-const app = require("./src/app");
-
-connectDb();
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
